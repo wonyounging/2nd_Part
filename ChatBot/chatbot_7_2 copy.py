3163facb80f9fbbbfada54aca6599fd887d252ed
wonyoung <ljll1231@naver.com>
2023-06-26 17:56:14 +0900

    230626_s_b

--- ChatBot/chatbot_7_2 copy.py
+++ ChatBot/chatbot_7_2 copy.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pandas as pd
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras import preprocessing
+import random
 
 # 데이터 읽어오기
 train_file = "./ChatBot/data/Chatbot_data-master/ChatbotData.csv"
@@ -25,18 +26,31 @@ ds = ds.shuffle(len(features))
 test_ds = ds.take(2000).batch(20)
 
 # 감정 분류 CNN 모델 불러오기
-model = load_model('./ChatBot/model/cnn_model_v2.h5')
-model.summary()
-model.evaluate(test_ds, verbose=2)
+model_v2 = load_model('./ChatBot/model/cnn_model_v2.h5')
+model_v2.summary()
+model_v2.evaluate(test_ds, verbose=2)
+
+model_v3 = load_model('./ChatBot/model/cnn_model_v3.h5')
+model_v3.summary()
+model_v3.evaluate(test_ds, verbose=2)
+
+print('데이터셋의 갯수 : ', len(corpus))
+random_int = random.randint(0,11823)
+print('테스트용 데이터셋 {}번째'.format(random_int))
 
 # 테스트용 데이터셋의 10212번째 데이터 출력
-print("단어 시퀀스 : ", corpus[10212])
-print("단어 인덱스 시퀀스 : ", padded_seqs[10212])
-print("문장 분류(정답) : ", labels[10212])
+print("단어 시퀀스 : ", corpus[random_int])
+print("단어 인덱스 시퀀스 : ", padded_seqs[random_int])
+print("문장 분류(정답) : ", labels[random_int])
 
 # 테스트용 데이터셋의 10212번째 데이터 감정 예측
-picks = [10212]
-predict = model.predict(padded_seqs[picks])
-predict_class = tf.math.argmax(predict, axis=1)
-print("감정 예측 점수 : ", predict)
-print("감정 예측 클래스 : ", predict_class.numpy())
+picks = [random_int]
+predict_v2 = model_v2.predict(padded_seqs[picks])
+predict_class_v2 = tf.math.argmax(predict_v2, axis=1)
+print("v2 감정 예측 점수 : ", predict_v2)
+print("v2 감정 예측 클래스 : ", predict_class_v2.numpy())
+
+predict_v3 = model_v3.predict(padded_seqs[picks])
+predict_class_v3 = tf.math.argmax(predict_v3, axis=1)
+print("v3 감정 예측 점수 : ", predict_v3)
+print("v3 감정 예측 클래스 : ", predict_class_v3.numpy())
